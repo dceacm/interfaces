a4841351e26506282dd38c7b07b92451c66040b8
DariusBerianClase <dariusberian@salesianoslosboscos.com>
2020-12-09 20:03:44 +0100

    darius

--- interfaces/interfaces/Form1.Designer.cs
+++ interfaces/interfaces/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Consulta = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_Alta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Consulta
+            // 
+            this.btn_Consulta.Location = new System.Drawing.Point(316, 119);
+            this.btn_Consulta.Name = "btn_Consulta";
+            this.btn_Consulta.Size = new System.Drawing.Size(75, 23);
+            this.btn_Consulta.TabIndex = 5;
+            this.btn_Consulta.Text = "Consultar";
+            this.btn_Consulta.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Alta
+            // 
+            this.btn_Alta.Location = new System.Drawing.Point(316, 54);
+            this.btn_Alta.Name = "btn_Alta";
+            this.btn_Alta.Size = new System.Drawing.Size(75, 23);
+            this.btn_Alta.TabIndex = 3;
+            this.btn_Alta.Text = "Alta";
+            this.btn_Alta.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Consulta);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Alta);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Consulta;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_Alta;
     }
 }
 
