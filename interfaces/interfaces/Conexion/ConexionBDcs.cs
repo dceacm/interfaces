1b13f8721784255854f6bcf7110ec788c33e2d79
Diego <dcena@hiberus.com>
2020-12-21 16:54:09 +0100

    cambios en conexionBD

--- interfaces/interfaces/Conexion/ConexionBDcs.cs
+++ interfaces/interfaces/Conexion/ConexionBDcs.cs
@@ -18,11 +18,9 @@ namespace interfaces.Conexion
          
             using (SqlConnection con = new SqlConnection(connectionString))
             using (SqlCommand cmd = new SqlCommand(queryString, con))
-            {
-              
+            {            
                 cmd.Parameters.AddWithValue("@carpeta", int.Parse(documento.Carpeta));
               
-
                 con.Open();
                 cmd.ExecuteNonQuery();
             }
@@ -33,6 +31,7 @@ namespace interfaces.Conexion
             bool anadir = false;
             AnadirCarpeta(documento);
             string queryString = string.Empty;
+
             if (string.IsNullOrEmpty(documento.Clave2))
             {
                 queryString = "INSERT Documento VALUES(@carpeta,@ordenCarpeta, @fecha, @contenido, @clave1, null, null);";
@@ -74,7 +73,8 @@ namespace interfaces.Conexion
         public string GetLastID(String text)
         {
             string id = string.Empty;
-            string queryString = "SELECT ISNULL((SELECT TOP 1 OrdenCarpeta FROM Documento Where carpeta=@carpeta ORDER BY OrdenCarpeta DESC),0);";
+            string queryString = "SELECT ISNULL((SELECT TOP 1 OrdenCarpeta FROM Documento WHERE carpeta=@carpeta ORDER BY OrdenCarpeta DESC),0);";
+
             using (SqlConnection con = new SqlConnection(connectionString))
             using (SqlCommand cmd = new SqlCommand(queryString, con))
             {
@@ -87,9 +87,11 @@ namespace interfaces.Conexion
 
         public void EliminarDocumento(Documento documento)
         {
-            string queryString = "DELETE FROM DOCUMENTO WHERE carpeta=@carpeta, ordenCarpeta=@ordenCarpeta, fecha=@fecha, " +
-                "contenido=@contenido, clave1=@clave1, clave2=@clave2, clave3=@clave3;)";
-            int count=CountDocumentos(documento.Carpeta);
+            string queryString = "DELETE DOCUMENTO WHERE carpeta=@carpeta AND ordenCarpeta=@ordenCarpeta AND fecha=@fecha AND " +
+                                    "contenido=@contenido AND clave1=@clave1;";
+
+            //int count=CountDocumentos(documento.Carpeta);
+
             using (SqlConnection con = new SqlConnection(connectionString))
             using (SqlCommand cmd = new SqlCommand(queryString, con))
             {
@@ -98,8 +100,6 @@ namespace interfaces.Conexion
                 cmd.Parameters.AddWithValue("@fecha", documento.Fecha);
                 cmd.Parameters.AddWithValue("@contenido", documento.Contenido);
                 cmd.Parameters.AddWithValue("@clave1", documento.Clave1);
-                cmd.Parameters.AddWithValue("@clave2", documento.Clave2);
-                cmd.Parameters.AddWithValue("@clave3", documento.Clave3);
 
                 con.Open();
                 cmd.ExecuteNonQuery();
@@ -109,10 +109,11 @@ namespace interfaces.Conexion
 
         public void ModificarDocumento(Documento documentoOrigen, Documento documento)
         {
-            string queryString = "UPDATE DOCUMENTO SET carpeta=@carpeta, ordenCarpeta=@ordenCarpeta, fecha=@fecha, " +
-                "contenido=@contenido, clave1=@clave1, clave2=@clave2, clave3=@clave3 WHERE carpeta=@carpetaOr, " +
-                "ordenCarpeta=@ordenCarpetaOr, fecha=@fechaOr, contenido=@contenidoOr, clave1=@clave1Or, " +
-                "clave2=@clave2Or, clave3=@clave3Or";
+            string queryString = "UPDATE Documento SET carpeta=@carpeta, ordenCarpeta=@ordenCarpeta, fecha=@fecha, " +
+                                    "contenido=@contenido, clave1=@clave1, clave2=@clave2, clave3=@clave3 " +
+                                    "WHERE carpeta=@carpetaOr AND " +
+                                    "ordenCarpeta=@ordenCarpetaOr AND fecha=@fechaOr AND contenido=@contenidoOr AND " +
+                                    "clave1=@clave1Or AND clave2=@clave2Or AND clave3=@clave3Or;";
             
             using (SqlConnection con = new SqlConnection(connectionString))
             using (SqlCommand cmd = new SqlCommand(queryString, con))
